9f3d64f138be54b52641eabaa0b3f3b2c52e08c2
georgi-m <georgimirzoyan@gmail.com>
2018-02-09 18:42:35 +0400

    Added test for kmalloc() functionality.

---
+++ test.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+typedef unsigned long long u64;
+
+struct cma_alloc
+{
+        u64 virt_start_addr;
+        u64 phys_start_addr;
+        u64 buffer_size;
+};
+
+#define IOCTL_CMA_ALLOC  _IOR('A', 1, struct cma_alloc)
+
+int main()
+{
+        cma_alloc cma;
+        int fd = open("/dev/cma_module", O_RDWR);
+        if (-1 == fd) {
+                std::cout<<"Failed to open cma_module device file."<<std::endl;
+        }
+        std::cout << "before ioclt call: Virtual address: " <<cma.virt_start_addr<< std::endl;
+        cma.buffer_size = 4096;
+        ioctl(fd, IOCTL_CMA_ALLOC, &cma);
+        std::cout << "after ioctl call Virtual address: " <<std::hex<<cma.virt_start_addr<< std::endl;
+        std::cout << "after ioctl call Physical address: " <<std::hex<<cma.phys_start_addr<< std::endl;
+        close(fd);
+        return 0;
+}
